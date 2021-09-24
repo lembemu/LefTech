d055f26986a1fe7388381a9d030aa330cd9bacc0
lembemu <77379362+lembemu@users.noreply.github.com>
2021-09-24 11:26:25 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+console.log('Hello World,\nI am Frank Lembemu, the owner of this website(LefTech).');
+
+
+var block = document.getElementsByClassName('block');
+var packages = document.getElementsByClassName('package');
+
+var i;
+for (var i = 0; i < block.length; i++) {
+  block[i].onclick = function(){
+    this.classList.toggle('active');
+    this.nextElementSibling.classList.toggle('show');
+  }
+}
+
+
+var backTop = $(".btn-back_to_top");
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 400) {
+    backTop.css('visibility', 'visible');
+  }
+  else if ($(document).scrollTop() < 400) {
+    backTop.css('visibility', 'hidden');
+  }
+});
+
+backTop.click(function() {
+  $('html').animate({
+    scrollTop: 0
+  }, 1000);
+  return false;
+});
